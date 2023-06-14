444ca15234697525ff026c7dd773c706e93f7194
daysond <daysond@outlook.com>
2023-06-13 22:58:21 -0400

    change uri to service name

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -20,7 +20,7 @@ export const useSignup = () => {
 
         console.log("[DEBUG] Signing up...")
 
-        const response = await fetch('http://localhost:4000/api/user/signup', {
+        const response = await fetch('http://api-server:4000/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
