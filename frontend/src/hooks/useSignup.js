fe57f0f63a227f0394939db3ecec9028e2c1ff6a
daysond <daysond@outlook.com>
2023-06-17 00:59:10 -0400

    changed fonts

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -20,7 +20,7 @@ export const useSignup = () => {
 
         console.log("[DEBUG] Signing up...")
 
-        const response = await fetch('http://159.203.11.15:4000/api/user/signup', {
+        const response = await fetch('http://localhost:4000/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
