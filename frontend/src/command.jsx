79da50664909f44864a7cdc1b623a50b6e1a7647
daysond <daysond@outlook.com>
2023-06-21 02:46:40 -0400

    fixed autofocus and auto scroll bug, added animation after finishing challenge-1

--- frontend/src/command.jsx
+++ frontend/src/command.jsx
@@ -1,31 +1,35 @@
-export const help = [
-  <div className="cmd-group" key="help-cmd-group">
-    <span className="command cmd-width">clear</span> - Clear console
-    <br />
-    <span className="command cmd-width">ls</span> - List directory contents
-    <br />
-    <span className="command cmd-width">cd</span> - Change directory [dir_name]
-    <br />
-    <span className="command cmd-width">cat</span> - Print file [file_name]
-    <br />
-    <span className="command cmd-width">edit</span> - Open file in the editor
-    [file_name]
-    <br />
-    <span className="command cmd-width">request</span> - Request challenge
-    <br />
-    <span className="command cmd-width">status</span> - print progress
-    <br />
-    <span className="command cmd-width">submit</span> - Submit final solution
-    for accessment [file_name]
-    <br />
-    <span className="command cmd-width">verify</span> - Run tests on solution
-    file [file_name]
-    <br />
-    <span className="command cmd-width">logout</span> - Log out
-    <br />
-  </div>,
-];
+import { nanoid } from "nanoid";
 
+export const Help = () => {
+  return (
+    <div className="cmd-group">
+      <span className="command cmd-width">clear</span> - Clear console
+      <br />
+      <span className="command cmd-width">ls</span> - List directory contents
+      <br />
+      <span className="command cmd-width">cd</span> - Change directory
+      [dir_name]
+      <br />
+      <span className="command cmd-width">cat</span> - Print file [file_name]
+      <br />
+      <span className="command cmd-width">edit</span> - Open file in the editor
+      [file_name]
+      <br />
+      <span className="command cmd-width">request</span> - Request challenge
+      <br />
+      <span className="command cmd-width">status</span> - print progress
+      <br />
+      <span className="command cmd-width">submit</span> - Submit final solution
+      for accessment [file_name]
+      <br />
+      <span className="command cmd-width">verify</span> - Run tests on solution
+      file [file_name]
+      <br />
+      <span className="command cmd-width">logout</span> - Log out
+      <br />
+    </div>
+  );
+};
 export const start = (
   <div className="cmd-group">
     <p>
@@ -121,17 +125,13 @@ export const AllTestPassed = () => {
 };
 
 export const TestCaseResult = (results) => {
-
-  const resultElement = Object.values(results)[0].map(r=> 
-    <p key={r.test}> 
-      <span className={ r.result === 'passed' ? "term-green" : "term-warning"}>
+  const resultElement = Object.values(results)[0].map((r) => (
+    <p key={r.test}>
+      <span className={r.result === "passed" ? "term-green" : "term-warning"}>
         {r.test}: {r.result}
-        </span>
-    </p>)
+      </span>
+    </p>
+  ));
 
-  return (
-    <div className="cmd-group">
-     {resultElement}
-    </div>
-  );
-}
+  return <div className="cmd-group">{resultElement}</div>;
+};
